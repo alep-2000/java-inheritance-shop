9a8a0cf5dd3c29a8ddd83dc3656d5a1f514ef5d0
alep-2000 <alessiopiras056@gmail.com>
2023-11-14 15:35:01 +0100

    Creazione domande all'utente e condizioni in base al prodotto

--- src/org/java/shop/Carrello.java
+++ src/org/java/shop/Carrello.java
@@ -1,5 +1,7 @@
 package org.java.shop;
 
+import java.util.Scanner;
+
 public class Carrello {
 
 	public static void main(String[] args) {
@@ -15,5 +17,58 @@ public class Carrello {
 		
 		Cuffie c = new Cuffie(1103801204, "Cuffia", "Cuffie JBL", 1200, 4, "Rosso", true, false);
 		System.out.println(c);
+		
+		while(true) {
+			Scanner in = new Scanner(System.in);
+			System.out.print("Vuoi inserire un nuovo prodotto? ");
+			String strP = in.nextLine();
+			
+			System.out.print("Inserisci il codice: ");
+			String strCodice = in.nextLine();
+			
+			System.out.print("Inserisci il nome del prodotto: ");
+			String strNome = in.nextLine();
+			
+			System.out.print("Inserisci la descrizione: ");
+			String strDescrizione = in.nextLine();
+			
+			System.out.print("Inserisci il prezzo del prodotto: ");
+			String strPrezzo = in.nextLine();
+			
+			System.out.print("Inserisci l'iva: ");
+			String strIva = in.nextLine();
+			
+			System.out.print("Quale? Smartphone/Televisori/Cuffie ");
+			String strQp = in.nextLine();
+			
+			if(strQp.equals("Smartphone")) {
+				System.out.print("Inserisci il codice IMEI: ");
+				String strIMEI = in.nextLine();
+				
+				System.out.print("Inserisci la grandezza della memoria: ");
+				String strKm = in.nextLine();
+			}
+			
+			if(strQp.equals("Televisore")) {
+				System.out.print("Inserisci le dimensioni: ");
+				String strDimensioni = in.nextLine();
+				
+				System.out.print("Inserisci se è smart: (SI/NO)");
+				String strSmart = in.nextLine();
+			}
+			
+			if(strQp.equals("Cuffie")) {
+				System.out.print("Inserisci il colore: ");
+				String strColore = in.nextLine();
+				
+				System.out.print("Inserisci se è wireless: (SI/NO");
+				String strWireless = in.nextLine();
+				
+				System.out.print("Inserisci se è cablata: (SI/NO");
+				String strCablata = in.nextLine();
+			}
+			
+			in.close();
+		}
 	}
 }
