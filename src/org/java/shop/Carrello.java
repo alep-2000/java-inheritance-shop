192fa9b8a369b4d07ee337364e3f47a3373ea935
alep-2000 <alessiopiras056@gmail.com>
2023-11-14 18:33:04 +0100

    Esercizio base finito

--- src/org/java/shop/Carrello.java
+++ src/org/java/shop/Carrello.java
@@ -1,106 +1,120 @@
 package org.java.shop;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class Carrello {
+	public class Carrello {
 
-	public static void main(String[] args) {
-		
-		Prodotto p = new Prodotto(102002093, "Smartphone", "Modello Samsung ultra performante", 300, 20);
-		System.out.println(p);
-		
-		Smartphone s = new Smartphone(102002093, "Smartphone", "Modello Samsung ultra performante", 300, 20,"101020020020020202",32);
-		System.out.println(s);
-		
-		Televisori t = new Televisori(1012010291, "Televisore", "Televisore ultra piatto", 800, 10, 65, true);
-		System.out.println(t);
-		
-		Cuffie c = new Cuffie(1103801204, "Cuffia", "Cuffie JBL", 1200, 4, "Rosso", true, false);
-		System.out.println(c);
-		
-		boolean nuovoProdotto = false;
-		boolean smart = false;
-		boolean wireless = false;
-		boolean cablate = false;
-		
-		while(true) {
-			Scanner in = new Scanner(System.in);
-			System.out.print("Vuoi inserire un nuovo prodotto? (SI/NO)");
-			String strP = in.nextLine();
-			
-			if(strP.equals("SI")) {
-				nuovoProdotto = true;
-			}
-			
-			if(strP.equals("NO"))break;
-			
-			
-			System.out.print("Inserisci il codice: ");
-			String strCodice = in.nextLine();
-			
-			System.out.print("Inserisci il nome del prodotto: ");
-			String strNome = in.nextLine();
-			
-			System.out.print("Inserisci la descrizione: ");
-			String strDescrizione = in.nextLine();
-			
-			System.out.print("Inserisci il prezzo del prodotto: ");
-			String strPrezzo = in.nextLine();
-			
-			System.out.print("Inserisci l'iva: ");
-			String strIva = in.nextLine();
-			
-			int[] prodotto = new int[5];
-			
-			for(int i= 0; i<prodotto.length; i++) {
-				System.out.println(strCodice + strNome + strDescrizione + strPrezzo + strIva);
-			}
-			
-			System.out.print("Quale? Smartphone/Televisori/Cuffie ");
-			String strQp = in.nextLine();
-			
-			
-			if(strQp.equals("Smartphone")) {
-				System.out.print("Inserisci il codice IMEI: ");
-				String strIMEI = in.nextLine();
-				
-				System.out.print("Inserisci la grandezza della memoria: ");
-				String strKm = in.nextLine();
-			}
-			
-			if(strQp.equals("Televisore")) {
-				System.out.print("Inserisci le dimensioni: ");
-				String strDimensioni = in.nextLine();
-				
-				System.out.print("Inserisci se è smart: (SI/NO)");
-				String strSmart = in.nextLine();
-				
-				if(strSmart.equals("SI")) {
-					smart = true;
-				}
-			}
-			
-			if(strQp.equals("Cuffie")) {
-				System.out.print("Inserisci il colore: ");
-				String strColore = in.nextLine();
-				
-				System.out.print("Inserisci se è wireless: (SI/NO");
-				String strWireless = in.nextLine();
-				
-				if(strWireless.equals("SI")) {
-					wireless = true;
-				}
-				
-				System.out.print("Inserisci se è cablata: (SI/NO");
-				String strCablata = in.nextLine();
-				
-				if(strCablata.equals("SI")) {
-					cablate = true;
-				}
-			}
-			
-			in.close();
-		}
+	    public static void main(String[] args) {
+	    	final int PRODUCT_COUNT = 5;
+	        boolean smart = false;
+	        boolean wireless = false;
+	        boolean cablate = false;
+	        String strIMEI = null;
+	        int intMemoria = 0;
+	        int intDimensioni = 0;
+	        String strColore = null;
+	        int i = 0;
+	        Prodotto[] prodotti = new Prodotto[PRODUCT_COUNT];
+	        
+
+	        Scanner in = new Scanner(System.in);
+	        while (true) {
+	            System.out.print("Vuoi inserire un nuovo prodotto? (SI/NO)");
+	            String strP = in.nextLine().toLowerCase();
+
+
+
+	            if (strP.equals("no"))break;
+	          
+	            System.out.print("Quale? Smartphone/Televisori/Cuffie ");
+	            String strQp = in.nextLine();
+
+	            System.out.print("Inserisci il codice: ");
+	            String strCodice = in.nextLine();
+	            int intCodice = Integer.valueOf(strCodice);
+
+	            System.out.print("Inserisci il nome del prodotto: ");
+	            String strNome = in.nextLine();
+
+	            System.out.print("Inserisci la descrizione: ");
+	            String strDescrizione = in.nextLine();
+
+	            System.out.print("Inserisci il prezzo del prodotto: ");
+	            String strPrezzo = in.nextLine();
+	            int intPrezzo = Integer.valueOf(strPrezzo);
+
+	            System.out.print("Inserisci l'iva: ");
+	            String strIva = in.nextLine();
+	            int intIva = Integer.valueOf(strIva);
+
+	            if (strQp.equals("Smartphone")) {
+	                System.out.print("Inserisci il codice IMEI: ");
+	                strIMEI = in.nextLine();
+
+	                System.out.print("Inserisci la grandezza della memoria: ");
+	                String strMemoria = in.nextLine();
+	                intMemoria = Integer.valueOf(strMemoria);
+	            } else if (strQp.equals("Televisore")) {
+	                System.out.print("Inserisci le dimensioni: ");
+	                String strDimensioni = in.nextLine();
+	                intDimensioni = Integer.valueOf(strDimensioni);
+
+	                System.out.print("Inserisci se è smart: (SI/NO)");
+	                String strSmart = in.nextLine().toLowerCase();
+
+	                if (strSmart.equals("si")) {
+	                    smart = true;
+	                }
+	            }else if(strQp.equals("Cuffie")) {
+	                System.out.print("Inserisci il colore: ");
+	                strColore = in.nextLine();
+
+	                System.out.print("Inserisci se è wireless: (SI/NO");
+	                String strWireless = in.nextLine().toLowerCase();
+
+	                if (strWireless.equals("si")) {
+	                    wireless = true;
+	                }
+
+	                System.out.print("Inserisci se è cablata: (SI/NO");
+	                String strCablata = in.nextLine().toLowerCase();
+
+	                if (strCablata.equals("si")) {
+	                    cablate = true;
+	                }
+	            }
+
+	           
+	            
+	            String CapitalizedProductType = strQp.substring(0, 1).toUpperCase() + strQp.substring(1).toLowerCase();
+	            
+	            switch (strQp) {
+	                case "Smartphone": {
+	                    
+						prodotti[i] = new Smartphone(intCodice, strNome, strDescrizione, intPrezzo, intIva, strIMEI, intMemoria);
+	                    i++;
+						break;
+	                }
+	                case "Televisore": {
+	                    
+						prodotti[i] = new Televisori(intCodice, strNome, strDescrizione, intPrezzo, intIva, intDimensioni, smart);
+						i++;
+						break;
+	                }
+	                case "Cuffie": {
+	                    
+						
+						prodotti[i] = new Cuffie(intCodice, strNome, strDescrizione, intPrezzo, intIva, strColore, wireless, cablate);
+						i++;
+						break;
+	                }
+	                default:
+						throw new IllegalArgumentException("Unexpected value: " + CapitalizedProductType);
+					}
+	        }
+	        
+	        for (int j = 0; j <i; j++) {
+	            System.out.println(prodotti[j] + "\n");
+	        }
+	    }
 	}
-}
